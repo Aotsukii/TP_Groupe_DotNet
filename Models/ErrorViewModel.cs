2a6b6eb1140f9428440d8439cfc0ec0f11b6fd3b
Buck <40405549+Buck@users.noreply.github.com>
2020-03-03 13:58:39 +0100

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tp_Groupe.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
